6dc48589deb295571934ec929e2047ccf32fea92
r-best <bobbyisbest3@gmail.com>
2017-06-10 10:51:33 -0400

    Trying out SDP

--- Client_Unix.py
+++ Client_Unix.py
@@ -2,14 +2,18 @@ import bluetooth
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-server_sock.bind(("", 21997))
+server_sock.bind(("", 0))
 server_sock.listen(1)
+print("Listening...")
 
-client_sock, address = server_sock.accept()
-print("Received connection from " + str(address))
+bluetooth.advertise_service(server_sock, "Test Service", "abcd")
 
-data = client_sock.recv(1024)
-print("Received " + str(data))
+while True:
+    client_sock, address = server_sock.accept()
+    print("Received connection from " + str(address))
+
+    data = client_sock.recv(1024)
+    print("Received " + str(data))
 
 client_sock.close()
 server_sock.close()
--- Device.py
+++ Device.py
@@ -1,29 +1,42 @@
 import bluetooth
 
-print("Searching for bluetooth-enabled client devices...\n")
+# print("Searching for bluetooth-enabled client devices...\n")
+#
+# while True:
+#     devices = bluetooth.discover_devices(duration=10, lookup_names=True)
+#
+#     if devices.__len__() != 0:
+#         break
+#     print("No devices found, retrying...")
+#
+# if devices.__len__() == 1:
+#     print(str(devices.__len__()) + " device discovered")
+# else:
+#     print(str(devices.__len__()) + " devices discovered")
+#
+# i = 0
+# for addr, name in devices:
+#     print(str(i) + " - " + name + '(' + addr + ')')
+#     i += 1
+#
+# client_index = input("Please enter the number (shown above) of the device you would like to connect to:")
+# client_addr = devices[client_index][0]
 
-while True:
-    devices = bluetooth.discover_devices(duration=10, lookup_names=True)
+service_matches = bluetooth.find_service(uuid="abcd")
 
-    if devices.__len__() != 0:
-        break;
-    print("No devices found, retrying...")
+if len(service_matches) == 0:
+    print("Couldn't find the service")
 
-if devices.__len__() == 1:
-    print(str(devices.__len__()) + " device discovered")
-else:
-    print(str(devices.__len__()) + " devices discovered")
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
 
-i = 0
-for addr, name in devices:
-    print(str(i) + " - " + name + '(' + addr + ')')
-    i += 1
-
-client_index = input("Please enter the number (shown above) of the device you would like to connect to:")
-client_addr = devices[client_index][0]
+print("Connecting to " + str(name) + " on " + str(host))
 
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((client_addr, 21997))
+sock.connect((host, port))
+# sock.connect((client_addr, 20))
 
 sock.send("Back to the future")
 
