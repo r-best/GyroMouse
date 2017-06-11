857f6fe3b774d162be07e2bf98d75b36b618e2f2
r-best <bobbyisbest3@gmail.com>
2017-06-11 12:00:34 -0400

    Test

--- Client_Unix.py
+++ Client_Unix.py
@@ -1,40 +1,13 @@
-# import bluetooth
-#
-# server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-#
-# server_sock.bind(("", 0))
-# server_sock.listen(1)
-# print("Listening...")
-#
-# bluetooth.advertise_service(sock=server_sock, name="Test Service", service_id="abcd")
-# print("Advertising service")
-#
-# while True:
-#     client_sock, address = server_sock.accept()
-#     print("Received connection from " + str(address))
-#
-#     data = client_sock.recv(1024)
-#     print("Received " + str(data))
-#
-# client_sock.close()
-# server_sock.close()
-
 import socket
-# from pymouse import PyMouse
-
-# mouse = PyMouse()
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.bind(('', 21997))
+server_sock.bind(('', 21998))
 server_sock.listen(1)
-print("listening on port 21997...")
+print("listening on port 21998...")
 
 client_sock, address = server_sock.accept()
 print("Received connection from " + str(address))
 
 while True:
     data = client_sock.recv(1024).decode()
-    print(data)#, data[1])
-
-    # current_pos = mouse.position()
-    # mouse.move(current_pos[0] + data[0], current_pos[1] + data[1])
+    print(data)
--- Device.py
+++ Device.py
@@ -1,47 +1,3 @@
-import bluetooth
-
-# print("Searching for bluetooth-enabled client devices...\n")
-#
-# while True:
-#     devices = bluetooth.discover_devices(duration=10, lookup_names=True)
-#
-#     if devices.__len__() != 0:
-#         break
-#     print("No devices found, retrying...")
-#
-# if devices.__len__() == 1:
-#     print(str(devices.__len__()) + " device discovered")
-# else:
-#     print(str(devices.__len__()) + " devices discovered")
-#
-# i = 0
-# for addr, name in devices:
-#     print(str(i) + " - " + name + '(' + addr + ')')
-#     i += 1
-#
-# client_index = input("Please enter the number (shown above) of the device you would like to connect to:")
-# client_addr = devices[client_index][0]
-
-# service_matches = bluetooth.find_service(uuid="abcd")
-#
-# if len(service_matches) == 0:
-#     print("Couldn't find the service")
-#
-# first_match = service_matches[0]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-#
-# print("Connecting to " + str(name) + " on " + str(host))
-#
-# sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-# sock.connect((host, port))
-# # sock.connect((client_addr, 20))
-#
-# sock.send("Back to the future")
-#
-# sock.close()
-
 import socket
 import time
 from sense_hat import SenseHat
@@ -51,7 +7,28 @@ def calibrate():
     new_zero = sense.get_orientation_degrees()
     global zero_pitch, zero_roll
     zero_pitch = new_zero["pitch"]
-    zero_roll = new_zero["roll"]
+    # zero_roll = new_zero["roll"]
+
+    # roll_split = (zero_roll + 180) % 360
+
+    left_bound = zero_pitch
+
+    total_range = range(0, 361)
+    neutral_range =
+    left_range =
+
+def calibrate_pitch(zero):
+    split = (zero + 180) % 360
+
+    left_bound = zero + 10
+    right_bound = zero - 10
+
+    if right_bound < 0:
+        right_bound = 360 + right_bound
+    if left_bound > 360:
+        left_bound = 0 + (left_bound - 360)
+
+    return range()
 
 
 def calculate_accel():
@@ -62,26 +39,20 @@ def calculate_accel():
     accel_horiz = 0
     # accel_vert = 0
 
-    if pitch < zero_pitch:
-        accel_horiz = -1
-    elif pitch > zero_pitch:
-        accel_horiz = 1
-
+    # if pitch < zero_pitch - 10:
+    #     accel_horiz = -1
+    # elif pitch > zero_pitch + 10:
+    #     accel_horiz = 1
+    #
     # if roll < zero_roll:
     #     accel_vert = -1
     # elif roll > zero_roll:
     #     accel_vert = 1
 
+
+
     return accel_horiz#, accel_vert
 
-
-def inc_notificator():
-    global note
-    note = note + 1
-    if note >= 10:
-        note = 0
-
-note = 0
 zero_pitch = 0
 zero_roll = 0
 
@@ -90,15 +61,16 @@ sense.set_imu_config(False, True, False)
 sense.low_light = True
 calibrate()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.0.14', 21997))
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect(('192.168.0.14', 21998))
 
 while True:
-    sense.show_letter(note)
-    inc_notificator()
-
     accel_horiz = calculate_accel()
-    sock.send(str(accel_horiz).encode())# + ':' + str(accel_vert)).encode())
-    time.sleep(.001)
+    # sock.send(str(accel_horiz).encode())
+
+    if sense.stick.get_events().__len__() > 0:
+        calibrate()
+
+    time.sleep(.01)
 
 sock.close()
